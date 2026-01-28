2191980e5d75a3e920c86218b82fd5cc6b9fc7e2
Meshack Yaro <yaromeshack@gmail.com>
2026-01-28 18:18:22 +0100

    feat: add crud implementation(updatePost)

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -38,8 +38,38 @@ const getPosts = async (req, res) => {
     }
 }
 
+const updatePost = async (req, res) => {
+    try {
+        
+        // check that request body is not empty
+        if (Object.keys(req.body).length === 0) res.status(400).json({
+            message: "Update data is required"
+        });
+
+        // find the post by id and update
+        const post = await Post.findByIdAndUpdate(req.params.id, req.body, 
+            {new: true});
+
+        if (!post) res.status(404).json({
+            message: "Post not found"
+        });
+        
+        res.status(200).json({
+            message: "Post updated successfully", 
+            post
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal Server Error", error
+        });
+    }
+}
+
+
 
 export {
     createPost,
-    getPosts
+    getPosts,
+    updatePost,
 };
